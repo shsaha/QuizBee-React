e1b6c8e6f21ead3cd72d652708d39963c7035547
Shaswata Saha <shsaha@ttu.ee>
2020-01-03 18:38:27 +0530

    All files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import quizService from './quizService'
+import QuestionBox from './components/QuestionBox'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    questionBank: [],
+    score: 0,
+    responses: 0
+  }
+
+  getQuestion = () => {
+    quizService().then(question => {
+      this.setState({
+        questionBank: question
+      })
+    })
+  }
+
+  computeAnswer = (answer, correctAnswer) => {
+    if (answer === correctAnswer) {
+      this.setState({
+        score: this.state.score + 1
+      })
+    }
+    this.setState({
+      responses: this.state.responses < 5 ? this.state.responses + 1 : 5
+    })
+  }
+  componentDidMount() {
+    this.getQuestion()
+  }
+  render() {
+    const { questionBank, responses, score } = this.state
+    return (
+      <div className="container">
+        <div className="title"></div>
+        {questionBank.length > 0 && responses < 5 && questionBank.map(({ question, answers, correct, questionId }) => (
+          <QuestionBox question={question} options={answers} key={questionId} selected={answer => this.computeAnswer(answer, correct)} />
+        ))}
+        {responses === 5 ? (<h2>{score}</h2>) : null}
+      </div>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/components/QuestionBox.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+export default function QuestionBox({ question, options }) {
+    const [answer, setAnswer] = useState(options)
+    return (
+        <div className="questionBox">
+            <div className="question">{question}</div>
+            {answer.map((text, index) => (<button key={index} className="answerBtn" onClick={() => {
+                setAnswer([text])
+            }}>{text}</button>))}
+        </div>
+    );
+}
